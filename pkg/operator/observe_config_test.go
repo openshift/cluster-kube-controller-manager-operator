d808f6042b264f74c93bb54652145e1116a985c7
Luis Sanchez <sanchezl@redhat.com>
2018-11-14 15:12:09 -0500

    observe network CIDRs

--- pkg/operator/observe_config_test.go
+++ pkg/operator/observe_config_test.go
@@ -1,7 +1,10 @@
 package operator
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/ghodss/yaml"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -38,4 +41,74 @@ func TestObserveCloudProviderNames(t *testing.T) {
 	if e, a := "aws", cloudProvider[0]; e != a {
 		t.Errorf("expected cloud-provider=%s, got %s", e, a)
 	}
+}
+
+func TestObserveClusterCIDRs(t *testing.T) {
+	indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+	indexer.Add(&corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster-config-v1",
+			Namespace: "kube-system",
+		},
+		Data: map[string]string{
+			"install-config": "networking:\n  podCIDR: podCIDR",
+		},
+	})
+	listers := Listers{
+		configmapLister: corelistersv1.NewConfigMapLister(indexer),
+	}
+	result, errs := observeClusterCIDRs(listers, map[string]interface{}{})
+	if len(errs) > 0 {
+		t.Fatal(errs)
+	}
+	expected := map[string]interface{}{
+		"extendedArguments": map[string]interface{}{
+			"cluster-cidr": []interface{}{
+				"podCIDR",
+			},
+		},
+	}
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("\n===== observed config expected:\n%v\n===== observed config actual:\n%v", toYAML(expected), toYAML(result))
+	}
+
+}
+
+func TestObserveServiceClusterIPRanges(t *testing.T) {
+	indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+	indexer.Add(&corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster-config-v1",
+			Namespace: "kube-system",
+		},
+		Data: map[string]string{
+			"install-config": "networking:\n  serviceCIDR: serviceCIDR",
+		},
+	})
+	listers := Listers{
+		configmapLister: corelistersv1.NewConfigMapLister(indexer),
+	}
+	result, errs := observeServiceClusterIPRanges(listers, map[string]interface{}{})
+	if len(errs) > 0 {
+		t.Fatal(errs)
+	}
+	expected := map[string]interface{}{
+		"extendedArguments": map[string]interface{}{
+			"service-cluster-ip-range": []interface{}{
+				"serviceCIDR",
+			},
+		},
+	}
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("\n===== observed config expected:\n%v\n===== observed config actual:\n%v", toYAML(expected), toYAML(result))
+	}
+
+}
+
+func toYAML(o interface{}) string {
+	b, e := yaml.Marshal(o)
+	if e != nil {
+		return e.Error()
+	}
+	return string(b)
 }
