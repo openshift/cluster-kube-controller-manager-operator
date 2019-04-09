da1dabaf139a9d824eca009a3a101bbd79669430
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-04-09 16:39:52 +0200

    Add auth-delegator cluster role binding

--- pkg/operator/targetconfigcontroller/targetconfigcontroller.go
+++ pkg/operator/targetconfigcontroller/targetconfigcontroller.go
@@ -132,6 +132,7 @@ func createTargetConfigController(c TargetConfigController, recorder events.Reco
 		"v3.11.0/kube-controller-manager/ns.yaml",
 		"v3.11.0/kube-controller-manager/kubeconfig-cm.yaml",
 		"v3.11.0/kube-controller-manager/leader-election-rolebinding.yaml",
+		"v3.11.0/kube-controller-manager/auth-delegator-rolebinding.yaml",
 		"v3.11.0/kube-controller-manager/svc.yaml",
 		"v3.11.0/kube-controller-manager/sa.yaml",
 	)
