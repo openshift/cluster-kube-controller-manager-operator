87dbf4063f9d0a012ff63e3fe1e9573cf13552ba
Maciej Szulik <maszulik@redhat.com>
2019-09-10 17:53:25 +0200

    bump(*)

--- vendor/golang.org/x/tools/go/analysis/passes/printf/types.go
+++ vendor/golang.org/x/tools/go/analysis/passes/printf/types.go
@@ -37,14 +37,8 @@ func matchArgTypeInternal(pass *analysis.Pass, t printfArgType, typ types.Type, 
 			return true // probably a type check problem
 		}
 	}
-
-	// %w accepts only errors.
-	if t == argError {
-		return types.ConvertibleTo(typ, errorType)
-	}
-
 	// If the type implements fmt.Formatter, we have nothing to check.
-	if isFormatter(typ) {
+	if isFormatter(pass, typ) {
 		return true
 	}
 	// If we can use a string, might arg (dynamically) implement the Stringer or Error interface?
