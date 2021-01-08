ddcd4d6d8c8e84895770e6513cd8618c1cd3c2d2
Maciej Szulik <maszulik@redhat.com>
2021-01-08 16:28:16 +0100

    Bug 1841119: apply extendedArguments directly to kcm in bootstrap

--- pkg/cmd/render/render_test.go
+++ pkg/cmd/render/render_test.go
@@ -3,17 +3,23 @@ package render
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
 	"reflect"
 	"sort"
+	"strconv"
 	"strings"
 	"testing"
 
+	"github.com/ghodss/yaml"
 	"github.com/google/go-cmp/cmp"
 	"github.com/spf13/cobra"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 var (
@@ -118,6 +124,8 @@ func TestRenderCommand(t *testing.T) {
 		args          []string
 		expectedErr   error
 		expectedFiles []string
+		// {filename: {field: value}}
+		expectedContents map[string]map[string]interface{}
 	}{
 		{
 			name: "no-flags",
@@ -146,6 +154,44 @@ func TestRenderCommand(t *testing.T) {
 				"manifests/manifests/00_openshift-kube-controller-manager-operator-ns.yaml",
 				"manifests/manifests/secret-csr-signer-signer.yaml",
 				"manifests/manifests/secret-initial-kube-controller-manager-service-account-private-key.yaml",
+			},
+			expectedContents: map[string]map[string]interface{}{
+				"manifests/bootstrap-manifests/kube-controller-manager-pod.yaml": {
+					"spec.containers[0].args": []interface{}{
+						"--openshift-config=/etc/kubernetes/config/kube-controller-manager-config.yaml",
+						"--kubeconfig=/etc/kubernetes/secrets/kubeconfig",
+						"--logtostderr=false",
+						"--alsologtostderr",
+						"--v=2",
+						"--log-file=/var/log/bootstrap-control-plane/kube-controller-manager.log",
+						"--allocate-node-cidrs=false",
+						"--authentication-kubeconfig=/etc/kubernetes/secrets/kubeconfig",
+						"--authorization-kubeconfig=/etc/kubernetes/secrets/kubeconfig",
+						"--cert-dir=/var/run/kubernetes",
+						"--cluster-signing-cert-file=/etc/kubernetes/secrets/kubelet-signer.crt",
+						"--cluster-signing-key-file=/etc/kubernetes/secrets/kubelet-signer.key",
+						"--configure-cloud-routes=false",
+						"--controllers=*",
+						"--controllers=-bootstrapsigner",
+						"--controllers=-tokencleaner",
+						"--controllers=-ttl",
+						"--enable-dynamic-provisioning=true",
+						"--experimental-cluster-signing-duration=720h",
+						"--flex-volume-plugin-dir=/etc/kubernetes/kubelet-plugins/volume/exec",
+						"--kube-api-burst=300",
+						"--kube-api-qps=150",
+						"--leader-elect-resource-lock=configmaps",
+						"--leader-elect-retry-period=3s",
+						"--leader-elect=true",
+						"--port=0",
+						"--pv-recycler-pod-template-filepath-hostpath=",
+						"--pv-recycler-pod-template-filepath-nfs=",
+						"--root-ca-file=/etc/kubernetes/secrets/kube-apiserver-complete-server-ca-bundle.crt",
+						"--secure-port=10257",
+						"--service-account-private-key-file=/etc/kubernetes/secrets/service-account.key",
+						"--use-service-account-credentials=true",
+					},
+				},
 			},
 		},
 	}
@@ -194,8 +240,58 @@ func TestRenderCommand(t *testing.T) {
 			if err != nil {
 				t.Errorf("file %q: %v", f, err)
 			}
+			if file, ok := test.expectedContents[f]; ok {
+				data, err := ioutil.ReadFile(p)
+				if err != nil {
+					t.Errorf("error reading file %s: %v", p, err)
+					continue
+				}
+				dataJSON, err := yaml.YAMLToJSON(data)
+				if err != nil {
+					t.Errorf("error converting file %s: %v", p, err)
+					continue
+				}
+				configObj, err := runtime.Decode(unstructured.UnstructuredJSONScheme, dataJSON)
+				if err != nil {
+					t.Errorf("error decoding %s: %v", p, err)
+					continue
+				}
+				config := configObj.(*unstructured.Unstructured)
+				for field, expectedValue := range file {
+					actualValue, err := readPath(config.Object, field)
+					if err != nil {
+						t.Errorf("error reading field %s: %v", field, err)
+						continue
+					}
+					if !reflect.DeepEqual(actualValue, expectedValue) {
+						t.Errorf("error comparing %s: \n%s\nvs\n%s\n", field, expectedValue, actualValue)
+						continue
+					}
+				}
+			}
 		}
 	}
+}
+
+func readPath(obj map[string]interface{}, path string) (interface{}, error) {
+	if strings.Contains(path, "[") {
+		nestedPath := strings.Split(path, "[")
+		outerObj, err := readPath(obj, nestedPath[0])
+		if err != nil {
+			return nil, err
+		}
+		index, err := strconv.Atoi(string(nestedPath[1][0]))
+		if err != nil {
+			return nil, err
+		}
+		newObj := outerObj.([]interface{})
+		return readPath(newObj[index].(map[string]interface{}), nestedPath[1][3:])
+	}
+	actualValue, found, err := unstructured.NestedFieldNoCopy(obj, strings.Split(path, ".")...)
+	if !found && err != nil {
+		return nil, fmt.Errorf("error reading field %s: %v", path, err)
+	}
+	return actualValue, nil
 }
 
 func TestDiscoverRestrictedCIDRsFromNetwork(t *testing.T) {
