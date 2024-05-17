b60a6863b50a3420ef3b1fba82a6ff3bd63ab21b
Vu Dinh <vudinh@outlook.com>
2024-05-16 22:36:35 -0400

    Revert cloud-config configmap deletion
    Signed-off-by: Vu Dinh <vudinh@outlook.com>

--- vendor/github.com/openshift/library-go/pkg/operator/configobserver/cloudprovider/observe_cloudprovider.go
+++ vendor/github.com/openshift/library-go/pkg/operator/configobserver/cloudprovider/observe_cloudprovider.go
@@ -1,8 +1,10 @@
 package cloudprovider
 
 import (
+	"k8s.io/apimachinery/pkg/api/errors"
 	corelisterv1 "k8s.io/client-go/listers/core/v1"
 
+	configv1 "github.com/openshift/api/config/v1"
 	configlistersv1 "github.com/openshift/client-go/config/listers/config/v1"
 	"github.com/openshift/library-go/pkg/operator/configobserver"
 	"github.com/openshift/library-go/pkg/operator/events"
@@ -47,13 +49,40 @@ func (c *cloudProviderObserver) ObserveCloudProviderNames(genericListers configo
 	listers := genericListers.(InfrastructureLister)
 	var errs []error
 
-	// Use a blank resource location to delete the old, unused cloud-config configmap, if it exists.
-	_ = listers.ResourceSyncer().SyncConfigMap(
+	infrastructure, err := listers.InfrastructureLister().Get("cluster")
+	if errors.IsNotFound(err) {
+		recorder.Warningf("ObserveCloudProviderNames", "Required infrastructures.%s/cluster not found", configv1.GroupName)
+		return existingConfig, errs
+	}
+	if err != nil {
+		return existingConfig, append(errs, err)
+	}
+
+	sourceCloudConfigMap := infrastructure.Spec.CloudConfig.Name
+	sourceCloudConfigNamespace := configNamespace
+
+	// If a managed cloud-provider config is available, it should be used instead of the default. If the configmap is not
+	// found, the default values should be used.
+	if _, err = listers.ConfigMapLister().ConfigMaps(machineSpecifiedConfigNamespace).Get(machineSpecifiedConfig); err == nil {
+		sourceCloudConfigMap = machineSpecifiedConfig
+		sourceCloudConfigNamespace = machineSpecifiedConfigNamespace
+	} else if !errors.IsNotFound(err) {
+		return existingConfig, append(errs, err)
+	}
+
+	sourceLocation := resourcesynccontroller.ResourceLocation{
+		Namespace: sourceCloudConfigNamespace,
+		Name:      sourceCloudConfigMap,
+	}
+
+	if err := listers.ResourceSyncer().SyncConfigMap(
 		resourcesynccontroller.ResourceLocation{
 			Namespace: c.targetNamespaceName,
 			Name:      "cloud-config",
 		},
-		resourcesynccontroller.ResourceLocation{})
+		sourceLocation); err != nil {
+		return existingConfig, append(errs, err)
+	}
 
 	return existingConfig, errs
 }
