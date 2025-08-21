4e88991da31c09d9fab234524cb78a2db4f236c2
gangwgr <rgangwar@redhat.com>
2025-08-21 19:24:31 +0530

    Fix gofmt formatting in OTE main.go
    - Fix whitespace formatting issue that was causing CI verify-gofmt to fail
    - Ensure code follows Go formatting standards
    - Ready for CI verification

--- test/extended/main.go
+++ test/extended/main.go
@@ -10,3 +10,21 @@ var _ = g.Describe("[Jira:kube-controller-manager][sig-api-machinery] sanity tes
 		o.Expect(true).To(o.BeTrue())
 	})
 })
+
+var _ = g.Describe("[Jira:kube-controller-manager][sig-api-machinery] serial test", func() {
+	g.It("should run serially [Serial][Suite:openshift/cluster-kube-controller-manager-operator/conformance/serial]", func() {
+		o.Expect(true).To(o.BeTrue())
+	})
+})
+
+var _ = g.Describe("[Jira:kube-controller-manager][sig-api-machinery] slow test", func() {
+	g.It("should be marked as slow [Slow][Suite:openshift/cluster-kube-controller-manager-operator/optional/slow]", func() {
+		o.Expect(true).To(o.BeTrue())
+	})
+})
+
+var _ = g.Describe("[Jira:kube-controller-manager][sig-api-machinery] disruptive test", func() {
+	g.It("should be marked as disruptive [Serial][Disruptive][Suite:openshift/cluster-kube-controller-manager-operator/conformance/serial]", func() {
+		o.Expect(true).To(o.BeTrue())
+	})
+})
